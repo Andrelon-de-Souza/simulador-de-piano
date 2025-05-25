dae1397fbc579698b8ce9884420137585246b8d2
Andrelon de Souza <dedeh.souza2019@gmail.com>
2025-05-24 23:57:35 -0300

    Projeto simulador de Piano finalizado com todas funcionalidades necessárias para uma boa experiência do usuário

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,12 +1,40 @@
 const teclasPiano = document.querySelectorAll(".teclas-piano .tecla");
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
 
+let teclasMapeadas = [];
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (tecla) => {
     audio.src = `src/tunes/${tecla}.wav`;
     audio.play();
+
+    const clickedKey = document.querySelector(`[data-key="${tecla}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150)
 };
 
 teclasPiano.forEach((tecla) => {
     tecla.addEventListener("click", () => playTune(tecla.dataset.key));
+    teclasMapeadas.push(tecla.dataset.key);
 });
+
+document.addEventListener("keydown", (e) => {
+    if(teclasMapeadas.includes(e.key)){
+        playTune(e.key)
+    }
+});
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+}
+
+const showHideKeys = () => {
+    teclasPiano.forEach(tecla => tecla.classList.toggle("hide"));
+}
+
+volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys);
